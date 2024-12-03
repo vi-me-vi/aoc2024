1cfb797de959c6e25fc59f44eeed878d7bffde3f
vi <viktor_melnyk@protonmail.com>
2024-12-03 22:01:21 +0100

    Cleanup debug values

--- src/dayrunner/day3.rs
+++ src/dayrunner/day3.rs
@@ -9,17 +9,24 @@ pub fn d3_run(input: String) -> String {
     let mut res1: i32 = 0;
     let split1 = input.split("mul(");
     for m in split1 {
-        let hold: Vec<&str>;
-        match m.split(")").next() {
-            Some(val) => hold = val.split(",").collect(),
+        let hold: Vec<&str>= match m.split(")").next() {
+            Some(val) => val.split(",").collect(),
             None => continue,
-        }
-        if hold.len() == 2 && hold[0].parse::<i32>().is_ok() && hold[1].parse::<i32>().is_ok() {
-            res1 += hold[0].parse::<i32>().unwrap() * hold[1].parse::<i32>().unwrap();
+        };
+        if hold.len() == 2 {
+            let a = match hold[0].parse::<i32>() {
+                Ok(val) => val,
+                Err(_) => continue,
+            };
+            let b = match hold[1].parse::<i32>() {
+                Ok(val) => val,
+                Err(_) => continue,
+            };
+            res1 += a * b;
         }
     }
 
-    logger.log(&format!("[day2] part one: {:?}", res1));
+    logger.log(&format!("[day3] part one: {:?}", res1));
 
     // PART 2
     let mut res2: i32 = 0;
@@ -29,24 +36,31 @@ pub fn d3_run(input: String) -> String {
         if i == 0 {
             line = l;
         } else {
-            match l.split_once("do()") {
-                Some(val) => line = val.1,
+            line = match l.split_once("do()") {
+                Some(val) => val.1,
                 None => continue,
             }
         }
         let split2_2 = line.split("mul(");
         for m in split2_2 {
-            let hold: Vec<&str>;
-            match m.split(")").next() {
-                Some(val) => hold = val.split(",").collect(),
+            let hold: Vec<&str> = match m.split(")").next() {
+                Some(val) => val.split(",").collect(),
                 None => continue,
-            }
-            if hold.len() == 2 && hold[0].parse::<i32>().is_ok() && hold[1].parse::<i32>().is_ok() {
-                res2 += hold[0].parse::<i32>().unwrap() * hold[1].parse::<i32>().unwrap();
+            };
+            if hold.len() == 2 {
+                let a = match hold[0].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => continue,
+                };
+                let b = match hold[1].parse::<i32>() {
+                    Ok(val) => val,
+                    Err(_) => continue,
+                };
+                res2 += a * b;
             }
         }
     }
-    logger.log(&format!("[day2] part two: {:?}", res2));
+    logger.log(&format!("[day3] part two: {:?}", res2));
 
 
     format!("Day 2:\n    PART 1: {:#?}\n    PART 2: {:#?}\n", res1, res2)
